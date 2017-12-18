f21025c6a3002b13de96c4882c02f8f652940599
t165491 <bill.wood@motoristsgroup.com>
2017-12-18 15:10:03 -0500

    Change static port to dynamic port

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ app.get('/servicenow', function(req, res){
     res.send('servicenow '+ req.query.string)
 })
 
-app.listen(3000, () => console.log('Express app listening on port 3000!'))
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Express app listening on port '+port+'!'))
