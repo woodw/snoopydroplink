6e0fd1d42851765be8f8a151a86397a669ddab55
t165491 <bill.wood@motoristsgroup.com>
2017-12-18 15:40:06 -0500

    Configure server to respond to slack request

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # snoopydroplink
 This is a test slack app that should take a string and returns a transformed string
+
+## Resources
+https://mager.co/how-to-write-a-slackbot-in-40-lines-of-code-52cf0c4fcf42
+https://devcenter.heroku.com/articles/heroku-cli#windows
+https://medium.com/@MVaragnat/how-to-build-a-slack-app-5b83d71e083a
--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/servicenow', function(req, res){ 
-    //console.log(req.query.string);
-    res.send('servicenow '+ req.query.string)
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Express app listening on port '+port+'!'))
+app.set('port', (process.env.PORT || 9001));
+
+app.get('/', function(req, res){
+  res.send('It works!');
+});
+
+app.post('/servicenow', function(req,res){
+    var body = {
+        response_type: "in_channel",
+        text: "I have to tell you something... but I forgot."
+    };
+    res.send(body);
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
